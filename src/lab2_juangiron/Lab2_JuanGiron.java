89a832e3a8c335911384a307a36268c583144cef
JuanGiron10 <gironjj18@gmail.com>
2025-05-02 14:08:42 -0600

    Segundo commit

--- src/lab2_juangiron/Lab2_JuanGiron.java
+++ src/lab2_juangiron/Lab2_JuanGiron.java
@@ -1,20 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab2_juangiron;
 
-/**
- *
- * @author admin
- */
+import java.util.Scanner;
+
 public class Lab2_JuanGiron {
 
-    /**
-     * @param args the command line arguments
-     */
+    Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner leer = new Scanner(System.in);
+        char resp = 's';
+        while (resp == 's' || resp == 'S') {
+            System.out.println("        MENU        ");
+            System.out.println("1. Piedra, pepel o tijera ");
+            System.out.println("        MENU        ");
+            System.out.println("        MENU        ");
+            int opcion = leer.nextInt();
+            if (opcion == 1) {
+                System.out.println(" Jugador 1. Elija una opcion: 1.piedra 2.papel 3.tijera ");
+                int jugador1 = leer.nextInt();
+                System.out.println(" Jugador 2. Elija una opcion: 1.piedra 2.papel 3.tijera ");
+                int jugador2 = leer.nextInt();
+                if (jugador1 == 1 && jugador2 == 2) {
+                    System.out.println("El jugador 2 gana");
+                } else if (jugador1 == 1 && jugador2 == 3) {
+                    System.out.println("El jugador 2 gana");
+                } else if (jugador1 == 2 && jugador2 == 1) {
+                    System.out.println("El jugador 2 gana");
+                } else if (jugador1 == 2 && jugador2 == 3) {
+                    System.out.println("El jugador 2 gana");
+                }else if (jugador1 == 3 && jugador2 == 1) {
+                    System.out.println("El jugador 2 gana");
+                }else if (jugador1 == 3 && jugador2 == 2) {
+                    System.out.println("El jugador 1 gana");
+                }else if (jugador1 == 1 && jugador2 == 1) {
+                    System.out.println("empate");
+                }else if (jugador1 == 2 && jugador2 == 2) {
+                    System.out.println("empate");
+                }else if (jugador1 == 3 && jugador2 == 3) {
+                    System.out.println("empate");
+                }
+            
+            }
+            if (opcion == 2) {
+                
+            }
+            
+        System.out.println("Desea volver al sistema?: " + resp);
+        resp = leer.next().charAt(0);
+        }
+
     
+    }
 }
